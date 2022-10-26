bdbe8939c7c24852d36affa7f47d236c6b6c43e0
Ryan Baker <ryan.andrew.baker@outlook.com>
2022-10-26 13:33:33 -0400

    Demo

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from KubeCon + Tanzu!";
+		return "Greetings from Spring Boot + Tanzu!";
 	}
 
 }
