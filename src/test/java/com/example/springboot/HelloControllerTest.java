a954f4b1a407eb72eeaf4c0cff6d89d49843598d
Ryan Baker <ryan.andrew.baker@outlook.com>
2023-01-24 00:09:29 -0600

    Demo

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Tanzu!", controller.index());
+        assertEquals("Greetings from Tanzu + Ryan!", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Tanzu!"));
+            .andExpect(content().string("Greetings from Tanzu + Ryan!"));
     }
 }
