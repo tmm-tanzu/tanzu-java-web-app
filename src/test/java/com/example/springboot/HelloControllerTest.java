74e3ef4034e96e5a2a874b25e5686b6d16d7d2c2
Ryan Baker <ryan.andrew.baker@outlook.com>
2023-01-23 23:14:32 -0600

    Demo

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from KubeCon + Tanzu!", controller.index());
+        assertEquals("Greetings from Tanzu!", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from KubeCon + Tanzu!"));
+            .andExpect(content().string("Greetings from Tanzu!"));
     }
 }
