b205a7153493fe43584d89d55d0dbbcb8c60a24f
Ryan Baker <ryan.andrew.baker@outlook.com>
2023-08-16 18:31:32 -0500

    Update Demo

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -26,6 +26,6 @@ class HelloControllerTest {
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from TAP + Demo!"));
+            .andExpect(content().string("Greetings from TAP + Triage!"));
     }
 }
