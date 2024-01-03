18dcb4bd71cd619998200b654ffcb8922e00ead8
Morgan Fine <mfine@mfineLWNQ6.vmware.com>
2024-01-02 19:23:29 -0800

    Add missing bean

---
+++ src/main/java/com/example/springboot/ApplicationInfoBean.java
@@ -0,0 +1,23 @@
+package com.example.springboot;
+
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+
+@Component
+public class ApplicationInfoBean {
+
+    @Value("${ami-id:N/A}")
+    private String amiId;
+
+    @Value("${hostname:N/A}")
+    public String hostname;
+
+    @Value("${instance-type:N/A}")
+    private String instanceType;
+
+    @Value("${services/domain:N/A}")
+    public String serviceDomain;
+
+    @Value("${placement/availability-zone:N/A}")
+    public String availabilityZone;
+}
