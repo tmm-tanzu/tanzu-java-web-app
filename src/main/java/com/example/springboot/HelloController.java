b28a268f5d0c22ef66a9af1ca0e84d3fe225a518
Morgan Fine <mfine@mfineLWNQ6.vmware.com>
2024-01-08 15:20:50 -0800

    Add timestamp to index page

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -3,6 +3,7 @@ package com.example.springboot;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.time.Instant;
 
 
 @Controller
@@ -19,6 +20,9 @@ public class HelloController {
         model.addAttribute("hostname", cloudMetadata.hostname);
         model.addAttribute("availabilityZone", cloudMetadata.availabilityZone);
         model.addAttribute("mac", cloudMetadata.mac);
+
+        Instant timestamp = Instant.now();
+        model.addAttribute("timestamp", timestamp);
 
         return "index";
     }
