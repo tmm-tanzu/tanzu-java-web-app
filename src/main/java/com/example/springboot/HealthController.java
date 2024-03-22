032e4ef3973b663b926f2935799372fe218045d6
Morgan Fine <mfine@mfineLWNQ6.vmware.com>
2024-03-22 10:16:51 -0700

    Add healthcheck controller

---
+++ src/main/java/com/example/springboot/HealthController.java
@@ -0,0 +1,27 @@
+package com.example.springboot;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@Controller
+public class HealthController {
+
+    private volatile boolean health = true;
+
+    @GetMapping("/health")
+    public ResponseEntity<Void> index() {
+        if (health) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.internalServerError().build();
+    }
+
+    @PostMapping("/health")
+    public void update(@RequestBody boolean health) {
+        this.health = health;
+    }
+}
