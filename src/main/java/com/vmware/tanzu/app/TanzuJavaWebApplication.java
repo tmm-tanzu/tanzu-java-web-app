74b8f93e5afed4da4d1a20e0fce750b9891a966e
Ben Hale <ben.hale@broadcom.com>
2024-08-19 09:01:45 -0700

    Support Cloud Foundry Service Bindings
    This change adds support for Cloud Foundry Service Bindings.  The
    application now includes information about their name and type, along
    with a fixed value for the provider.
    
    Signed-off-by: Ben Hale <ben.hale@broadcom.com>

--- src/main/java/com/vmware/tanzu/app/TanzuJavaWebApplication.java
+++ src/main/java/com/vmware/tanzu/app/TanzuJavaWebApplication.java
@@ -1,5 +1,6 @@
 package com.vmware.tanzu.app;
 
+import io.pivotal.cfenv.core.CfEnv;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,8 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.bindings.Bindings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import software.amazon.awssdk.imds.Ec2MetadataAsyncClient;
+
+import java.util.List;
 
 @SpringBootApplication
 public class TanzuJavaWebApplication {
@@ -21,8 +25,12 @@ public class TanzuJavaWebApplication {
     }
 
     @Bean
-    Mono<Bindings> bindings() {
-        return Mono.fromSupplier(Bindings::new);
+    Mono<List<ServiceBindingInfo>> serviceBindingInfo() {
+        return Flux.concat(
+                        Flux.fromIterable(new Bindings().getBindings()).map(ServiceBindingInfo::new),
+                        Flux.fromIterable(new CfEnv().findAllServices()).map(ServiceBindingInfo::new)
+                )
+                .collectList();
     }
 
     @Bean
@@ -30,10 +38,10 @@ public class TanzuJavaWebApplication {
         Ec2MetadataAsyncClient client = Ec2MetadataAsyncClient.create();
 
         return Mono.zip(
-                Mono.fromFuture(client.get("/latest/meta-data/placement/availability-zone")),
-                Mono.fromFuture(client.get("/latest/meta-data/hostname")),
-                Mono.fromFuture(client.get("/latest/meta-data/mac"))
-        )
+                        Mono.fromFuture(client.get("/latest/meta-data/placement/availability-zone")),
+                        Mono.fromFuture(client.get("/latest/meta-data/hostname")),
+                        Mono.fromFuture(client.get("/latest/meta-data/mac"))
+                )
                 .doFinally(t -> client.close())
                 .map(CloudInfo::new)
                 .onErrorResume(t -> {
