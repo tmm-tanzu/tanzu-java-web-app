69c71e15eb15579bddf956d966d98c19d2ace641
Ben Hale <ben.hale@broadcom.com>
2024-08-22 15:09:24 -0700

    Handle new bindings info strategy
    Signed-off-by: Ben Hale <ben.hale@broadcom.com>

--- src/main/java/com/vmware/tanzu/app/InfoController.java
+++ src/main/java/com/vmware/tanzu/app/InfoController.java
@@ -6,25 +6,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 @Controller
 class InfoController {
 
-    private final Mono<Bindings> bindings;
+    private final Mono<List<ServiceBindingInfo>> serviceBindingInfos;
     private final Mono<CloudInfo> cloudInfo;
     private final Mono<Info> info;
 
-    InfoController(Mono<Bindings> bindings, Mono<CloudInfo> cloudInfo, Mono<Info> info) {
-        this.bindings = bindings;
+    InfoController(Mono<List<ServiceBindingInfo>> serviceBindingInfos, Mono<CloudInfo> cloudInfo, Mono<Info> info) {
+        this.serviceBindingInfos = serviceBindingInfos;
         this.cloudInfo = cloudInfo;
         this.info = info;
     }
 
     @GetMapping("/")
     Mono<Rendering> info() {
-        return Mono.zip(bindings, cloudInfo, info)
+        return Mono.zip(serviceBindingInfos, cloudInfo, info)
                 .map(t -> Rendering
                         .view("info")
-                        .modelAttribute("bindings", t.getT1())
+                        .modelAttribute("serviceBindingInfos", t.getT1())
                         .modelAttribute("cloudInfo", t.getT2())
                         .modelAttribute("info", t.getT3())
                         .build());
